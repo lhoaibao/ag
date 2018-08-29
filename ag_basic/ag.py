277ef2791abc3159371c87a34f09ee8e7d19c3e2
LHOAIBAO <lehoaibao081999@gmail.com>
2018-08-29 18:12:00 +0700

    new

--- ag_basic/ag.py
+++ ag_basic/ag.py
@@ -5,75 +5,42 @@ import os
 def makeLine(t, argv, mode):
     if mode == 0:
         if argv in t:
-            nd = nd.replace(argv, '\033[30;43m' + argv + '\033[0m')
+            t = t.replace(argv, '\033[30;43m' + argv + '\033[0m')
     if mode == 1:
         check = []
+        word = ''
         if argv.lower() in t.lower():
             for i in range(len(t)):
-                a = i
-                j = 0
-                w = ''
-                while argv[j].lower() == t[a].lower():
-                    if j != len(argv) - 1:
-                        w += t[a]
-                        a += 1
-                        j += 1
-                    else:
-                        if argv[j].lower() == t[a].lower():
-                            w += t[a]
-                            if w not in check:
-                                check.append(w)
-                                t = t.replace(w, '\033[30;43m' + w + '\033[0m')
-                            break
-                        else:
-                            break
+                word = t[i:i + len(argv)]
+                if word.lower() == argv.lower():
+                    if word not in check:
+                        check.append(word)
+                        t = t.replace(word, '\033[30;43m' + word + '\033[0m')
     return t
 
 
 def caseSensitive(item):
     if sys.argv[1] == '--case-sensitive':
-        if len(sys.argv) < 4:
-            files = os.listdir()
-            for file in files:
-                if os.path.isfile(file):
-                    print('\033[1;32m' + file + '\033[0m')
-                    file = open(file, "r")
-                    content = file.read()
-                    content = content.split('\n')
-                    for i in range(len(content)-1):
-                        if sys.argv[2] in content[i]:
-                            print('\033[1;33m' + str(i+1)+'\033[0m', end=":")
-                            print(makeLine(content[i], sys.argv[2], 0))
-                    file.close()
-        elif os.path.isfile(sys.argv[3]):
-            file = open(sys.argv[3], "r")
-            content = file.read()
-            content = content.split('\n')
-            for i in range(len(content)-1):
-                if sys.argv[2] in content[i]:
-                    print('\033[1;33m' + str(i+1)+'\033[0m', end=":")
-                    print(makeLine(content[i], sys.argv[2], 0))
+        if os.path.isfile(sys.argv[3]):
+            changeFile(sys.argv[3], sys.argv[2], 0)
     else:
-        if len(sys.argv) < 3:
-            files = os.listdir()
-            for file in files:
-                if os.path.isfile(file):
-                    print('\033[1;32m'+file + '\033[0m')
-                    file = open(file, "r")
-                    content = file.read()
-                    content = content.split('\n')
-                    for i in range(len(content)-1):
-                        if sys.argv[1].lower() in content[i].lower():
-                            print('\033[1;33m' + str(i+1)+'\033[0m', end=":")
-                            print(makeLine(content[i], sys.argv[1], 1))
-        elif os.path.isfile(sys.argv[2]):
-            file = open(sys.argv[2], "r")
-            content = file.read()
-            content = content.split('\n')
-            for i in range(len(content)-1):
-                if sys.argv[1].lower() in content[i].lower():
+        if os.path.isfile(sys.argv[2]):
+            changeFile(sys.argv[2], sys.argv[1], 1)
+
+
+def changeFile(file,item,mode):
+        file = open(file, "r")
+        content = file.read()
+        content = content.split('\n')
+        for i in range(len(content)-1):
+            if mode == 1:
+                if item.lower() in content[i].lower():
                     print('\033[1;33m' + str(i+1)+'\033[0m', end=":")
-                    print(makeLine(content[i], sys.argv[1], 1))
+                    print(makeLine(content[i], item, mode))
+            if mode == 0:
+                if item in content[i]:
+                    print('\033[1;33m' + str(i+1)+'\033[0m', end=":")
+                    print(makeLine(content[i], item, mode))
 
 
 caseSensitive(sys.argv)
--- ag_directories/ag.py
+++ ag_directories/ag.py
@@ -8,62 +8,71 @@ def makeLine(t, argv, mode):
             t = t.replace(argv, '\033[30;43m' + argv + '\033[0m')
     if mode == 1:
         check = []
+        word = ''
         if argv.lower() in t.lower():
             for i in range(len(t)):
-                a = i
-                j = 0
-                w = ''
-                while argv[j].lower() == t[a].lower():
-                    if j != len(argv) - 1:
-                        w += t[a]
-                        a += 1
-                        j += 1
-                    else:
-                        if argv[j].lower() == t[a].lower():
-                            w += t[a]
-                            if w not in check:
-                                check.append(w)
-                                t = t.replace(w, '\033[30;43m' + w + '\033[0m')
-                            break
-                        else:
-                            break
+                word = t[i:i + len(argv)]
+                if word.lower() == argv.lower():
+                    if word not in check:
+                        check.append(word)
+                        t = t.replace(word, '\033[30;43m' + word + '\033[0m')
     return t
 
 
 def main(item):
-    if sys.argv[1] == '--case-sensitive':
-        if len(sys.argv) < 4:
-            openFile(sys.argv[2], 0)
-        elif os.path.isfile(sys.argv[3]):
-            changeFile(sys.argv[3],sys.argv[2],0)
-    if sys.argv[1] == '--hidden':
-        openFile(sys.argv[2], 0)
-    else:
-        if len(sys.argv) < 3:
-            openFile(sys.argv[1], 1)
-        elif os.path.isfile(sys.argv[2]):
-            changeFile(sys.argv[2],sys.argv[1],1)
+    if '--case-sensitive' not in sys.argv and '--hidden' not in sys.argv:
+        if not os.path.isfile(sys.argv[-1]):
+            openFile(sys.argv[-2], 1, 0)
+        else:
+            changeFile(sys.argv[-1], sys.argv[-2], 0)
+    elif sys.argv[-2] == '--case-sensitive' or sys.argv[-2] == '--hidden':
+        if '--case-sensitive' in sys.argv and '--hidden' in sys.argv:
+            openFile(sys.argv[-1], 0, 1)
+        elif '--case-sensitive' in sys.argv:
+            openFile(sys.argv[-1], 0, 0)
+        else:
+            openFile(sys.argv[-1], 1, 1)
+    elif sys.argv[-3] == '--case-sensitive' or sys.argv[-3] == '--hidden':
+        if not os.path.isfile(sys.argv[-1]):
+            if '--case-sensitive' in sys.argv and '--hidden' in sys.argv:
+                openFile(sys.argv[-2], 0, 1)
+            elif '--case-sensitive' in sys.argv:
+                openFile(sys.argv[-2], 0, 0)
+            else:
+                openFile(sys.argv[-2], 1, 1)
+        else:
+            if '--case-sensitive' in sys.argv and '--hidden' in sys.argv:
+                changeFile(sys.argv[-1], sys.argv[-2], 0)
+            elif '--case-sensitive' in sys.argv:
+                changeFile(sys.argv[-1], sys.argv[-2], 0)
+            else:
+                changeFile(sys.argv[-1], sys.argv[-2], 1)
 
 
-def openFile(item, mode):
+def openFile(item, mode, hidden):
     files = os.listdir()
-    for file in files:
-        if os.path.isfile(file):
+    if hidden == 1:
+        for file in files:
             print('\033[1;32m'+file + '\033[0m')
             changeFile(file,item,mode)
+    if hidden == 0:
+        for file in files:
+            if file[0] != '.':
+                print('\033[1;32m'+file + '\033[0m')
+                changeFile(file,item,mode)
 
 
 def changeFile(file,item,mode):
-    file = open(file, "r")
-    content = file.read()
-    content = content.split('\n')
-    for i in range(len(content)-1):
-        if mode == 1:
-            if item.lower() in content[i].lower():
-                print('\033[1;33m' + str(i+1)+'\033[0m', end=":")
-                print(makeLine(content[i], item, mode))
-        if mode == 0:
-            if item in content[i]:
-                print('\033[1;33m' + str(i+1)+'\033[0m', end=":")
-                print(makeLine(content[i], item, mode))
+        file = open(file, "r")
+        content = file.read()
+        content = content.split('\n')
+        for i in range(len(content)-1):
+            if mode == 1:
+                if item.lower() in content[i].lower():
+                    print('\033[1;33m' + str(i+1)+'\033[0m', end=":")
+                    print(makeLine(content[i], item, mode))
+            if mode == 0:
+                if item in content[i]:
+                    print('\033[1;33m' + str(i+1)+'\033[0m', end=":")
+                    print(makeLine(content[i], item, mode))
 main(sys.argv)
